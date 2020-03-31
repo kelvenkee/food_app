61887e544a8a966c31460590c1aa06fa35ea8f7b
kangweikiat@gmail.com <kangweikiat@gmail.com>
2020-03-31 18:06:27 +0800

    Initial commit

---
+++ lib/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _username = TextEditingController();
+  final _password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) => ListView(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 120,
+                ),
+                Image.asset('assets/login.png', scale: 1.5),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 60.0,
+                  ),
+                  TextFormField(
+                    controller: _username,
+                    decoration: InputDecoration(
+                      labelText: "Username",
+                      labelStyle: TextStyle(fontSize: 20),
+                      filled: true,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please Enter Username';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    controller: _password,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(fontSize: 20),
+                      filled: true,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please Enter Password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ButtonTheme(
+                    height: 50,
+                    disabledColor: Colors.blueAccent,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          bool result = _performLogin();
+                          if (result) {
+                            print("login success");
+                          } else {
+                            Scaffold.of(context).showSnackBar(new SnackBar(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                              content: new Text(
+                                'Invalid Username & Password',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                            ));
+                          }
+                        }
+                      },
+                      child: Text('Login',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Customer? Click here to go back')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool _performLogin() {
+    String username = _username.text;
+    String password = _password.text;
+
+    if (username == "admin" && password == "root") {
+      return true;
+    } else
+      return false;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Application',
+      home: LoginPage(),
+    );
+  }
+}
