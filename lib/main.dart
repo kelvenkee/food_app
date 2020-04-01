ad0a6afeaf9c37ab872cf071eff63d80595d34c5
wong <lkwong1997@gmail.com>
2020-04-01 14:03:46 +0800

    create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/login.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:new FoodApp(),
+  ));
+}
+
+
+class FoodApp extends StatefulWidget{
+  _FoodAppState createState() => _FoodAppState();
+}
+
+class _FoodAppState extends State <FoodApp>{
+  Widget build(BuildContext context){
+    return SplashScreen(
+      seconds: 5,
+      backgroundColor: Colors.white,
+      image: Image.asset("assets/delivery.gif"),
+      loadingText: Text("Presented by Team Coconut"),
+      loaderColor: Colors.black,
+      photoSize: 150.0,
+      navigateAfterSeconds: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
