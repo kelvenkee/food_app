7d49111d6bd8ddeaa37fc5c488171dad43f41577
kangweikiat@gmail.com <kangweikiat@gmail.com>
2020-05-05 15:26:51 +0800

    Define and handle the named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:food_app/Login/login.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:food_app/router.dart';
+// import 'package:food_app/Login/login.dart';
+import 'constant.dart';
 
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: new FoodApp(),
+    // home: new FoodApp(),
+    initialRoute: splashScreenRoute,
+    onGenerateRoute: createRoute,
   ));
 }
 
-class FoodApp extends StatefulWidget {
-  _FoodAppState createState() => _FoodAppState();
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return buildMaterialApp();
+//   }
 
-class _FoodAppState extends State<FoodApp> {
-  Widget build(BuildContext context) {
-    return buildSplashScreen();
-  }
-
-  SplashScreen buildSplashScreen() {
-    return SplashScreen(
-      seconds: 5,
-      backgroundColor: Colors.white,
-      image: Image.asset("assets/delivery.gif"),
-      loadingText: Text("Presented by Team Coconut"),
-      loaderColor: Colors.black,
-      photoSize: 150.0,
-      navigateAfterSeconds: MyApp(),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return buildMaterialApp();
-  }
-
-  MaterialApp buildMaterialApp() {
-    return MaterialApp(
-      title: 'Food Application',
-      debugShowCheckedModeBanner: false,
-      home: Login(),
-    );
-  }
-}
+//   MaterialApp buildMaterialApp() {
+//     return MaterialApp(
+//       title: 'Food Application',
+//       debugShowCheckedModeBanner: false,
+//       home: Login(),
+//     );
+//   }
+// }
---
+++ lib/router.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/Login/login.dart';
+import 'package:food_app/Login/login_page.dart';
+import 'package:food_app/Menu/admin_page.dart';
+import 'package:food_app/Menu/edit_menu.dart';
+import 'package:food_app/Menu/view_order.dart';
+import 'package:food_app/Settings/help_page.dart';
+import 'package:food_app/Settings/profile_page.dart';
+import 'package:food_app/Settings/setting.dart';
+import 'package:food_app/Settings/setting_password.dart';
+import 'package:food_app/constant.dart';
+import 'package:food_app/splashscreen.dart';
+
+Route<dynamic> createRoute(settings) {
+  switch (settings.name) {
+    case splashScreenRoute:
+      return MaterialPageRoute(builder: (context) => FirstSplashScreen());
+    case loginRoute:
+      return MaterialPageRoute(builder: (context) => Login());
+    case login_pageRoute:
+      return MaterialPageRoute(builder: (context) => LoginPage());
+    case admin_pageRoute:
+      return MaterialPageRoute(builder: (context) => AdminPage());
+    case edit_menuRoute:
+      return MaterialPageRoute(builder: (context) => EditMenu());
+    case view_orderRoute:
+      return MaterialPageRoute(builder: (context) => ViewOrder());
+    case help_pageRoute:
+      return MaterialPageRoute(builder: (context) => HelpPage());
+    case profile_pageRoute:
+      return MaterialPageRoute(builder: (context) => ProfilePage());
+    case setting_passwordRoute:
+      return MaterialPageRoute(builder: (context) => SettingPasswordPage());
+    case settingRoute:
+      return MaterialPageRoute(builder: (context) => SettingPage());
+  }
+  return null;
+}
