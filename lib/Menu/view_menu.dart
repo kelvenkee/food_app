213bff1f438d46ea1f09d3d894f5d05165035a7e
wong <lkwong1997@gmail.com>
2020-05-07 18:44:02 +0800

    swipe to delete is added in view_menu

--- lib/Menu/view_menu.dart
+++ lib/Menu/view_menu.dart
@@ -37,49 +37,65 @@ class _ViewMenuState extends State<ViewMenu> {
         title: Text("View Menu"),
       ),
       body: ListView.separated(
-          itemBuilder: (context, index) => ListTile(
-                contentPadding: const EdgeInsets.all(5.0),
-                title: Text(widget._foodItems[index].foodName,
-                    style: TextStyle(
-                        color: Colors.orange[900],
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold)),
-                subtitle: Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Price: RM" +
-                              widget._foodItems[index].unitPrice
-                                  .toStringAsFixed(2),
-                          style: TextStyle(
-                            color: Colors.black54,
-                            fontSize: 18.0,
-                          ),
-                          textAlign: TextAlign.justify,
-                        ),
-                        Text(
-                          widget._foodItems[index].foodDescription,
-                          style: TextStyle(
-                            color: Colors.black54,
-                            fontSize: 13.0,
-                          ),
-                          textAlign: TextAlign.justify,
-                        ),
-                      ]),
-                ),
-                trailing: Container(
-                  height: 80.0,
-                  width: 80.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.asset(widget._foodItems[index].imageName,
-                        fit: BoxFit.cover, alignment: Alignment.center),
-                  ),
-                ),
-                onTap: () => _navigate(index),
+          itemBuilder: (context, index) => Dismissible( //DELETE BY SWIPPING 
+            key: UniqueKey(), 
+            onDismissed: (direction){
+              setState(() {
+                widget._foodItems.removeAt(index);
+                Scaffold.of(context).showSnackBar(new SnackBar(
+                  content: new Text( widget._foodItems[index].foodName + " was deleted."),
+                ));
+              });
+            },
+            background: new Container(
+              color: Colors.redAccent,
+              child: Icon(Icons.delete, color: Colors.white, size: 40),
+              alignment: AlignmentDirectional.centerEnd,
               ),
+                      child: ListTile(
+                  contentPadding: const EdgeInsets.all(5.0),
+                  title: Text(widget._foodItems[index].foodName,
+                      style: TextStyle(
+                          color: Colors.orange[900],
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold)),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Price: RM" +
+                                widget._foodItems[index].unitPrice
+                                    .toStringAsFixed(2),
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18.0,
+                            ),
+                            textAlign: TextAlign.justify,
+                          ),
+                          Text(
+                            widget._foodItems[index].foodDescription,
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 13.0,
+                            ),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ]),
+                  ),
+                  trailing: Container(
+                    height: 80.0,
+                    width: 80.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset(widget._foodItems[index].imageName,
+                          fit: BoxFit.cover, alignment: Alignment.center),
+                    ),
+                  ),
+                  onTap: () => _navigate(index),
+                ),
+          ),
           separatorBuilder: (context, index) => Divider(
                 color: Colors.blueGrey,
               ),
